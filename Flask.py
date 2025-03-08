a40f81dfc1f2267cc56ea2d73258b59d429c11cc
ikarpi <dkarpenko1499@gmail.com>
2025-03-09 02:15:48 +0300

    add render_template

--- Flask.py
+++ Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask,
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def first():
 
 @app.route("/bombardir/")
 def players():
-    return render_template("Dz.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
